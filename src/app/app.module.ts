0856f118eb6766df7b1d0911f4efc0173bc7277f
gierrmann <thisisgie.21@gmail.com>
2018-06-28 06:01:43 +0800

    added search box, added materialize datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { DatepickerComponent } from './datepicker/datepicker.component';
+import { MatFormFieldModule } from '@angular/material';
+import { MatNativeDateModule  } from '@angular/material';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DatepickerComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
